556f5f3ae93c5ae5271679d921df90767ce8621c
akiakiS <35886966+akiakishitai@users.noreply.github.com>
2020-03-29 23:21:00 +0900

    fix: fisher.fishの方を読み込ませる

--- plugin-install.fish
+++ plugin-install.fish
@@ -1,7 +1,7 @@
 #!/usr/bin/fish
 
 curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
-source ~/.config/fish/config.fish
+source ~/.config/fish/functions/fisher.fish
 
 fisher add jethrokuan/z; \
     and fisher add jethrokuan/fzf; \
